624267898a5d16dda021b1785bb4a51e286cf5be
Nicolas <developer.imagine@gmail.com>
2018-07-09 22:36:06 -0300

    Primer commit

---
+++ softcomputacion/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace softcomputacion
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/main.css",
+                      "~/Content/css/util.min.css"));
+        }
+    }
+}
---
+++ softcomputacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="softcomputacion.MvcApplication" Language="C#" %>
