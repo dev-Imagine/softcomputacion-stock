b6ddf55293e2e08aa80d8f03db2236438b43c332
Nicolas <developer.imagine@gmail.com>
2018-07-11 16:56:55 -0300

    Casi terminado proveedor
    Falta una parte del eliminar y modificar

--- softcomputacion/Controllers/ProveedorController.cs
+++ softcomputacion/Controllers/ProveedorController.cs
@@ -3,15 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using softcomputacion.Models;
+using softcomputacion.Servicios;
 
 namespace softcomputacion.Controllers
 {
     public class ProveedorController : Controller
     {
-        // GET: Proveedores
+        [HttpGet]
         public ActionResult ListaProveedores()
         {
-            return View();
+            try
+            {
+                srvProveedor sProveedor = new srvProveedor();
+                ViewBag.lstProveedores = sProveedor.ObtenerProveedores();
+                return View();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Error", new { stError = "Se produjo un error al intentar obtener los proveedores." });
+            }
+        }
+        [HttpPost]
+        public ActionResult ListaProveedores(string nombreEmpresa)
+        {
+            try
+            {
+                srvProveedor sProveedor = new srvProveedor();
+                ViewBag.lstProveedores = sProveedor.ObtenerProveedores(nombreEmpresa);
+                return View();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Error", new { stError = "Se produjo un error al intentar obtener los proveedores." });
+            }
+        }
+
+        //vistas parciales
+
+        //metodos
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult GuardarModificarProveedor(proveedor oProveedor)
+        {
+            try
+            {
+                srvProveedor sProveedor = new srvProveedor();
+                sProveedor.GuardarModificarProveedor(oProveedor);
+                return RedirectToAction("ListaProveedores");
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Error", new { stError = "Se produjo un error al intentar guardar o modificar el proveedor." });
+            }
         }
     }
 }
---
+++ softcomputacion/Servicios/srvProveedor.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using softcomputacion.Models;
+
+namespace softcomputacion.Servicios
+{
+    public class srvProveedor
+    {
+        private proveedor GuardarProveedor(proveedor oProveedor)
+        {
+            try
+            {
+                using (BDSoftComputacionEntities bd = new BDSoftComputacionEntities())
+                {
+                    oProveedor.nombreEmpresa = oProveedor.nombreEmpresa.ToUpper();
+                    bd.Entry(oProveedor).State = System.Data.Entity.EntityState.Added;
+                    bd.SaveChanges();
+                    return oProveedor;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        private proveedor ModificarProveedor(proveedor oProveedor)
+        {
+            try
+            {
+                using (BDSoftComputacionEntities bd = new BDSoftComputacionEntities())
+                {
+                    oProveedor.nombreEmpresa = oProveedor.nombreEmpresa.ToUpper();
+                    oProveedor.proveedorXproducto.Clear();
+                    bd.Entry(oProveedor).State = System.Data.Entity.EntityState.Modified;
+                    bd.SaveChanges();
+                    return oProveedor;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public proveedor GuardarModificarProveedor(proveedor oProveedor)
+        {
+            try
+            {
+                if (oProveedor.idProveedor==0)
+                {
+                    GuardarProveedor(oProveedor);
+                }
+                else
+                {
+                    ModificarProveedor(oProveedor);
+                }
+                return oProveedor;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<proveedor> ObtenerProveedores()
+        {
+            try
+            {
+                using (BDSoftComputacionEntities bd = new BDSoftComputacionEntities())
+                {
+                    return bd.proveedor.OrderBy(x => x.nombreEmpresa).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public List<proveedor> ObtenerProveedores(string nombreEmpresa)
+        {
+            try
+            {
+                using (BDSoftComputacionEntities bd = new BDSoftComputacionEntities())
+                {
+                    if (nombreEmpresa == "")
+                    {
+                        return bd.proveedor.ToList();
+                    }
+                    return bd.proveedor.Where(x => x.nombreEmpresa.Contains(nombreEmpresa.ToUpper())).OrderBy(x => x.nombreEmpresa).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
