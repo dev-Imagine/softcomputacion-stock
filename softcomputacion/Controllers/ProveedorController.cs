df1e00c4401c54cffa06f140f5a22a5bb91665e1
Nicolas <developer.imagine@gmail.com>
2018-07-11 00:38:26 -0300

    Primer comit de proveedor,

---
+++ softcomputacion/Controllers/ProveedorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace softcomputacion.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedores
+        public ActionResult ListaProveedores()
+        {
+            return View();
+        }
+    }
+}
