715b32d7a61c701a33346de296a0b5ec811c41a2
Nicolas <developer.imagine@gmail.com>
2018-07-12 20:22:01 -0300

    agrege seleccion de categoria y proveedor

--- softcomputacion/Controllers/ProductoController.cs
+++ softcomputacion/Controllers/ProductoController.cs
@@ -10,11 +10,44 @@ namespace softcomputacion.Controllers
 {
     public class ProductoController : Controller
     {
-        // GET: Producto
+        // *************** Vistas
         public ActionResult Producto()
         {
-            ViewBag.oProducto = new producto();
-            return View();
+            try
+            {
+                srvCategoria sCategoria = new srvCategoria();
+                ViewBag.lstCategorias = sCategoria.ObtenerCategorias();
+                srvProveedor sProveedor = new srvProveedor();
+                ViewBag.lstProveedores = sProveedor.ObtenerProveedores();
+                ViewBag.oProducto = new producto();
+                return View();
+            }
+            catch (Exception)
+            {
+                return RedirectToAction("Error", "Error", new { stError = "Se produjo un error al intentar obtener los datos del servidor." });
+            }
+            
+        }
+
+        // *************** Metodos
+        public JsonResult ObtenerSubcategoriaDeCategoria(int idCategoria)
+        {
+            try
+            {
+                srvCategoria sCategoria = new srvCategoria();
+                categoria oCategoria = sCategoria.ObtenerCategoria(idCategoria);
+                string stListaOption = "<option selected value=\"0\">Subcategoria</option>";
+                foreach (subcategoria oSubcategoria in oCategoria.subcategoria.ToList())
+                {
+                    stListaOption = stListaOption + "<option value=\"" + oSubcategoria.idSubCategoria + "\">" + oSubcategoria.nombre + "</option>";
+                }
+                return Json(stListaOption);
+            }
+            catch (Exception)
+            {
+                return Json("<option selected value=\"0\">Subcategoria</option>");
+            }
+            
         }
     }
 }
---
+++ softcomputacion/Servicios/srvCategoria.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using softcomputacion.Models;
+
+namespace softcomputacion.Servicios
+{
+    public class srvCategoria
+    {
+        public List<categoria> ObtenerCategorias()
+        {
+            try
+            {
+                using (BDSoftComputacionEntities bd = new BDSoftComputacionEntities())
+                {
+                    return bd.categoria.OrderBy(x => x.nombre).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                return new List<categoria>();
+            }
+        }
+        public categoria ObtenerCategoria(int idCategoria)
+        {
+            try
+            {
+                using (BDSoftComputacionEntities bd = new BDSoftComputacionEntities())
+                {
+                    categoria oCategoria = bd.categoria.Where(x => x.idCategoria == idCategoria).FirstOrDefault();
+                    oCategoria.subcategoria.Count();
+                    return oCategoria;
+                }
+            }
+            catch (Exception ex)
+            {
+                return new categoria();
+            }
+        }
+    }
+}
